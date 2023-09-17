8c9d064e6d71ca00d0b8798c3463f8ad96fa351d
Eleonora Mattera <m.eleonora98@live.it>
2023-09-17 14:40:37 +0200

    create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Row>
+      <Col xs={12}>All right are reserved</Col>
+    </Row>
+  );
+}
+
+export default Footer;
